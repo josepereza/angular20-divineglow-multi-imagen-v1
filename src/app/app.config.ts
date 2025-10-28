66a1c3a2e7b72c0cc40d73c69e36ed2fe2610502
josepereza <josepereza66@gmail.com>
2025-10-28 20:25:43 +0100

    first commit37

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,13 +3,14 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './admin/interceptors/auth-interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideRouter(routes,withComponentInputBinding()), provideClientHydration(withEventReplay()),
-    provideHttpClient(withFetch())
+    provideHttpClient(withFetch(),withInterceptors([authInterceptor]))
   ]
 };
